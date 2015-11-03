e042b0c166870b8d9c0c37e6d0a46b72167e7c06
Robert Gazda <rbgazdda@gmail.com>
2015-11-03 09:22:51 +0100

    czysty zend z doctrine i twig

---
+++ config/autoload/doctrine.local.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'doctrine' => [
+        'connection' => [
+            // default connection name
+            'orm_default' => [
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                'params' => [
+                    'host'     => 'localhost',
+                    'port'     => '3306',
+                    'user'     => 'root',
+                    'password' => '',
+                    'dbname'   => 'vallheru',
+                ]
+            ]
+        ],
+        'driver' => [
+            // defines an annotation driver with two paths, and names it `my_annotation_driver`
+            'my_annotation_driver' => [
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => [
+                    'module' //@TODO Ustawić odpowiednią scieżkę do katalogu z encjami
+                ],
+            ],
+            // default metadata driver, aggregates all other drivers into a single one.
+            // Override `orm_default` only if you know what you're doing
+            'orm_default' => [
+                'drivers' => [
+                    // register `my_annotation_driver` for any entity under namespace `My\Namespace`
+                    'My\Namespace' => 'my_annotation_driver'
+                ]
+            ]
+        ]
+    ]
+];
